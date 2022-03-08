0682a044001d1cdbfd7c83a801724b6be573b4b4
Niels Louwes <niels.louwes@and.digital>
2022-03-08 18:07:30 +0100

    Integration test for multiple div elements and div active and non actives states

--- src/components/Todo/__Test__/ToDo.test.js
+++ src/components/Todo/__Test__/ToDo.test.js
@@ -1,4 +1,5 @@
 import { render, screen, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { BrowserRouter } from 'react-router-dom';
 import Todo from '../Todo';
 
@@ -10,16 +11,50 @@ const MockToDo = () => {
     )
 }
 
+const addTask = (tasks) => {
+  const inputElement = screen.getByPlaceholderText(/Add a new task here.../i)
+  const buttonElement = screen.getByRole('button', { name: /Add/i});
+  tasks.forEach( task => {
+     fireEvent.change(inputElement, {target : {value: task}});
+     fireEvent.click(buttonElement);
+  } )
+}
+
 describe("Todo", () => {
-  it('information entered input element shows up in task list after pressing add button', () => {
+  // it('information entered input element shows up in task list after pressing add button', () => {
+  //   render(<MockToDo />);
+  //   addTask("Go grocery shopping")
+
+  //   const divElement = screen.getByText(/Go grocery shopping/i)
+
+  //   expect(divElement).toBeInTheDocument();
+  // });
+
+  it('when three tasks are entered as input, should render multiple todo div items', () => {
     render(<MockToDo />);
-    const inputElement = screen.getByPlaceholderText(/Add a new task here.../i)
-    const buttonElement = screen.getByRole('button', { name: /Add/i})
-    fireEvent.change(inputElement, {target : {value: "Go grocery shopping"}});
-    fireEvent.click(buttonElement);
+    addTask(["Go grocery shopping", "Clean my room", "Take my dog for a walk"]);
+
+    const divElement = screen.getAllByTestId("task-container");
+    
+    expect(divElement.length).toBe(3);
+  });
+
+  it('task should not have completed class when initially rendered', () => {
+    render(<MockToDo />);
+    addTask(["Go grocery shopping"]);
+
     const divElement = screen.getByText(/Go grocery shopping/i)
-    expect(divElement).toBeInTheDocument();
-    // const headingElement = screen.getByText(/my header/i);
-    // expect(headingElement).toBeInTheDocument();
+    
+    expect(divElement).not.toHaveClass("todo-item-active");
+  });
+
+  it('task is greyed out and lined out when clicked on', () => {
+    render(<MockToDo />);
+    addTask(["Go grocery shopping"]);
+
+    const divElement = screen.getByText(/Go grocery shopping/i)
+    fireEvent.click(divElement);
+    
+    expect(divElement).toHaveClass("todo-item-active");
   });
 })
